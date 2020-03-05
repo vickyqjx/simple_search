d234985692dee3d95cb24b32bcccdfd4f2512e55
Jingxian Qiu <jingxianqiu@unknown.home>
2020-03-05 21:03:11 +1100

    initial commit

---
+++ lib/zendesk_search.ex
@@ -0,0 +1,18 @@
+defmodule ZendeskSearch do
+  @moduledoc """
+  Documentation for ZendeskSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZendeskSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zendesk_search_test.exs
@@ -0,0 +1,8 @@
+defmodule ZendeskSearchTest do
+  use ExUnit.Case
+  doctest ZendeskSearch
+
+  test "greets the world" do
+    assert ZendeskSearch.hello() == :world
+  end
+end
